a6d0f69c34dd39640668783314d1651c361ac58f
aicherish0320 <aicherish0320@163.com>
2022-06-28 08:19:55 +0800

    删除链表节点

--- linked-list/review.js
+++ linked-list/review.js
@@ -1,7 +1,6 @@
 /**
-  反转链表
+  删除节点
  */
-
 const a = { val: 'a' }
 const b = { val: 'b' }
 const c = { val: 'c' }
@@ -11,33 +10,9 @@ a.next = b
 b.next = c
 c.next = d
 
-// 迭代
-const reverseList = (head) => {
-  let p1 = head
-  let p2 = null
-  while (p1) {
-    const temp = p1.next
-    p1.next = p2
-    p2 = p1
-    p1 = temp
-  }
-  return p2
+const deleteNode = (node) => {
+  node.val = node.next.val
+  node.next = node.next.next
 }
 
-// const ret = reverseList(a)
-
-// 递归
-const reverseList2 = (head) => {
-  if (!head || !head.next) {
-    return head
-  }
-
-  const newHead = reverseList2(head.next)
-
-  head.next.next = head
-  head.next = null
-  return newHead
-}
-
-const ret = reverseList2(a)
-console.log('ret >>> ', ret)
+deleteNode(b)
