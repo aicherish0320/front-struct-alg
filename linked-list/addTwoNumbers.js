87b64d7fdb764572a7794ba6100a927393e2a67e
aicherish0320 <aicherish0320@163.com>
2022-06-29 08:51:07 +0800

    两数相加

--- linked-list/addTwoNumbers.js
+++ linked-list/addTwoNumbers.js
@@ -3,4 +3,35 @@
   给你两个非空的链表，表示两个非负的整数。它们每个数组都是按照逆序的方式存储的，并且每个节点只能存储一位数字。
   请将两个数相加，并以相同形式返回一个表示和的链表
 */
-const addTwoNumbers = function (l1, l2) {}
+
+function ListNode(val = '') {
+  this.val
+}
+
+const addTwoNumbers = function (l1, l2) {
+  const ret = new ListNode(0)
+
+  let p1 = l1
+  let p2 = l2
+  let p3 = ret
+
+  let carry = 0
+
+  while (p1 || p2) {
+    let v1 = p1 ? p1.val : 0
+    let v2 = p2 ? p2.val : 0
+    let v = v1 + v2 + carry
+    carry = Math.floor(v / 10)
+    p3.next = new ListNode(v % 10)
+
+    p1 = p1 && p1.next
+    p2 = p2 && p2.next
+    p3 = p3.next
+  }
+
+  if (carry) {
+    p3.next = new ListNode(carry)
+  }
+
+  return ret.next
+}
--- linked-list/review.js
+++ linked-list/review.js
@@ -1,24 +1,39 @@
 /**
-  双指针遍历链表
+  两数相加
+  给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+  请你将两个数相加，并以相同形式返回一个表示和的链表。
+  2 -> 4 -> 3
+  5 -> 6 -> 4
  */
-const a = { val: 'a' }
-const b = { val: 'b' }
-const c = { val: 'c' }
-const d = { val: 'd' }
 
-a.next = b
-b.next = c
-c.next = d
-
-const fn = (head) => {
-  let p1 = head
-  let p2 = head.next
-
-  while (p1 && p2) {
-    console.log(p1.val, p2.val)
-    p1 = p2
-    p2 = p2.next
-  }
+function ListNode(val = '') {
+  this.val
 }
 
-fn(a)
+const fn = (l1, l2) => {
+  const ret = new ListNode(0)
+
+  let p1 = l1
+  let p2 = l2
+  let p3 = ret
+
+  let carry = 0
+
+  while (p1 || p2) {
+    let v1 = p1 ? p1.val : 0
+    let v2 = p2 ? p2.val : 0
+    let v = v1 + v2 + carry
+    carry = Math.floor(v / 10)
+    p3.next = new ListNode(v % 10)
+
+    p1 = p1 && p1.next
+    p2 = p2 && p2.next
+    p3 = p3.next
+  }
+
+  if (carry) {
+    p3.next = new ListNode(carry)
+  }
+
+  return ret.next
+}
