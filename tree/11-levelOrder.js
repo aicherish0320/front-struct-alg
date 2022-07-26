29fef31491e7404e20e80ad5fec01be67576668d
aicherish0320 <aicherish0320@163.com>
2022-07-26 22:41:18 +0800

    层序遍历2

--- tree/11-levelOrder.js
+++ tree/11-levelOrder.js
@@ -1,3 +1,23 @@
 /*
   层序遍历
 */
+
+const levelOrder = (root) => {
+  if (!root) return []
+
+  const ret = []
+  const q = [root]
+
+  while (q.length) {
+    let len = q.length
+    ret.push([])
+    while (len--) {
+      const n = q.shift()
+      ret[ret.length - 1].push(n.val)
+      n.left && q.push(n.left)
+      n.right && q.push(n.right)
+    }
+  }
+
+  return ret
+}
